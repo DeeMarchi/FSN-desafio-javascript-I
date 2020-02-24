871f5ec7721ed2a70c6d4d80e7cf98fc1bae0f9f
Denner Marchi <denner883@hotmail.com>
2020-02-24 01:21:31 -0300

    refatoração e implementação de adicionar aluno

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
-
+const alunosDaEscola = [{
+		nome: "Henrique",
+		cursos: [],
+	},
+	{
+		nome: "Edson",
+		cursos: [],
+	},
+	{
+		nome: "Bruno",
+		cursos: [],
+	},
+	{
+		nome: "Guilherme",
+		cursos: [],
+	},
+	{
+		nome: "Carlos",
+		cursos: [],
+	},
+	{
+		nome: "Lucca",
+		cursos: [{
+			nomeDoCurso: "UX",
+			dataMatricula: new Date('2019-12-17T12:30:00'),
+			notas: [8.1, 7.8, 9.5],
+			faltas: 0,
+		}],
+	}
+];
 
 // implementação
+
+const listarAlunos = listaDeAlunos => {
+	listaDeAlunos.forEach((aluno) => {
+		console.log(aluno);
+	});
+};
+
+const adicionarAluno = alunoNovo => {
+	let mensagemDeStatus = "";
+
+	if (!alunosDaEscola.find(alunoNoSistema => alunoNoSistema.nome === alunoNovo )) {
+		alunosDaEscola.push({
+			nome: alunoNovo,
+			cursos: [],
+		});
+		mensagemDeStatus = `Aluno ${alunoNovo} cadastrado com sucesso!`;
+	} else {
+		mensagemDeStatus = `O aluno ${alunoNovo} já está cadastrado no sistema`;
+	}
+	console.log(mensagemDeStatus);
+};
+
+adicionarAluno("Henrique");
+listarAlunos(alunosDaEscola);
+adicionarAluno("Daniel");
+listarAlunos(alunosDaEscola);
